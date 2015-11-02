dc800cf0b8c7410c55730332d3049e464143d2ac
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2015-11-03 00:38:01 +0900

    rm data

--- data/req.sh
+++
@@ -1,7 +0,0 @@
-curl -i -X GET \
--H 'Accept: application/vnd.travis-ci.2+json' \
--H 'User-Agent: MyClient/1.0.0' \
---data '{"token": "H3jJLd7x6Eejfv1FsUdD"}' \
-https://api.travis-ci.org/repos/jsk-ros-pkg/jsk_demos/builds/88499467  | sed -e 's/}/}\n/g' | sed -e 's/,/,\n/g'
-#--data '{"build_id": 88499467}' \
-#-H 'Authorization: token H3jJLd7x6Eejfv1FsUdD' \
