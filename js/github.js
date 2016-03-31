4eb9b5e425f5838e0ca18690ab82454c37877503
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-03-31 12:00:47 +0900

    fix: simplify show badge on github

--- js/github.js
+++ js/github.js
@@ -7,19 +7,13 @@ $(function(){
       chrome.runtime.sendMessage(
         { requestBadge: true },
         function(res) {
-          var xhr = new XMLHttpRequest();
-          xhr.responseType = "arraybuffer";
-          xhr.open("GET", res.badge_url, true);
-          xhr.onload = function(){
-            var data = new Uint8Array(this.response);
-            var oURL = URL.createObjectURL(new Blob([data], {type: "image/svg+xml" }));
-            var image = new Image();
-            image.onload = function() {
-              URL.revokeObjectURL(oURL);
-            };
-            image.src = oURL;
-            $('a[data-pjax=#js-repo-pjax-container]').after('<span style="vertical-align: middle; margin-left: 8px"><a href="' + res.travis_url + '"><img width="98" height="20" src="' + res.badge_url + '" /></a></span>');
+          var img = new Image();
+          img.onload = function(){
+            $('div.repohead-details-container h1.entry-title').append(img);
           };
+          img.src = res.badge_url;
+          img.height = 16;
+          $(img).css('margin-top', '4px');
         });
     }
   });
