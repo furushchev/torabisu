f5282022c39d50bbb3a220add17c2998e59e242a
Yuki Furuta <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2016-04-01 17:00:50 +0900

    move structure

---
+++ scripts/make-zip.sh
@@ -0,0 +1,9 @@
+B1;3409;0c#!/bin/bash
+
+if [ "$(basename `pwd`)" != "torabisu" ]; then
+    echo "please execute in \"torabisu\" directory"
+    exit 1
+fi
+
+git clean -dfix
+(cd  src; zip -r ../torabisu.zip ./*)
