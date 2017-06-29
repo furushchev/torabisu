d04c51f250a96fcf11e984803ad3ea5a3f445df9
Furushchev <furushchev@jsk.imi.i.u-tokyo.ac.jp>
2017-06-29 10:13:17 +0900

    [src/js/background.js] fix: parse error of travis url tracking query from github

--- src/js/background.js
+++ src/js/background.js
@@ -68,7 +68,7 @@ var fetchBuildInfo = function(repo, build_no, cb){
 var restartTravisContextMenuCallback = function(info, tab){
   if (info.menuItemId !== "restart_travis") return;
   var link_matched = info.linkUrl.match(/^https:\/\/travis-ci\.org\/([a-zA-Z0-9-_.]+)\/([a-zA-Z0-9-_.]+)\/jobs\/([0-9]+)$/);
-  var build_no = info.pageUrl.match(/^https:\/\/travis-ci\.org\/([a-zA-Z0-9-_.]+)\/([a-zA-Z0-9-_.]+)\/builds\/([0-9]+)$/)[3];
+  var build_no = info.pageUrl.match(/^https:\/\/travis-ci\.org\/([a-zA-Z0-9-_.]+)\/([a-zA-Z0-9-_.]+)\/builds\/([0-9]+).*$/)[3];
   var repo = link_matched[1] + '/' + link_matched[2];
   var job_no = link_matched[3];
   fetchBuildInfo(repo, build_no, function(build_info){
